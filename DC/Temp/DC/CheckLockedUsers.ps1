a7009679ee859e3fdcf207ce4d765e9fbdc81a31
Vabolos <79908586+Vabolos@users.noreply.github.com>
2024-10-11 11:02:12 +0200

    function added to check for elevated perms (restart PS if needed)

--- DC/Temp/DC/CheckLockedUsers.ps1
+++ DC/Temp/DC/CheckLockedUsers.ps1
@@ -1,3 +1,19 @@
+# Function to check if the script is running with sufficient privileges
+function Test-Admin {
+    $currentUser = [Security.Principal.WindowsIdentity]::GetCurrent()
+    $principal = New-Object Security.Principal.WindowsPrincipal($currentUser)
+    return $principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+}
+
+# If not running as administrator, restart the script with elevated privileges
+if (-not (Test-Admin)) {
+    Write-Host "Restarting PowerShell with elevated privileges..." -ForegroundColor Green
+    Start-Sleep -Seconds 1
+    # Relaunch PowerShell with 'Run as Administrator' and pass the script as an argument
+    Start-Process powershell -ArgumentList "-File `"$($MyInvocation.MyCommand.Definition)`"" -Verb RunAs
+    exit
+}
+
 Do {
     # Perform the AD account locked-out search
     Search-ADAccount -LockedOut
