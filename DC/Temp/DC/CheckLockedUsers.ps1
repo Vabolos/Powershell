3a20ad571ae7c73abe439f9214514eeba33689b1
Vabolos <79908586+Vabolos@users.noreply.github.com>
2024-10-11 11:00:46 +0200

    Do-While added to Locked users Check

--- DC/Temp/DC/CheckLockedUsers.ps1
+++ DC/Temp/DC/CheckLockedUsers.ps1
@@ -1,2 +1,14 @@
-Search-ADAccount -LockedOut
-pause
+Do {
+    # Perform the AD account locked-out search
+    Search-ADAccount -LockedOut
+
+    # Pause for user to see the results
+    pause
+
+    # Ask user if they want to restart or close the script
+    $response = Read-Host "Would you like to [R]estart or [C]lose the script?"
+
+    # Continue the loop if the user chooses 'R', exit if 'C'
+} While ($response -eq 'R' -or $response -eq 'r')
+
+Write-Host "Closing the script..."
