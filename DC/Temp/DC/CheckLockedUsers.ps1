7115fab4681bd8ea6776b8198cd56422f79e74a4
Vabolos <79908586+Vabolos@users.noreply.github.com>
2024-10-11 11:22:22 +0200

    Locked User check now clears host to prevent flooding

--- DC/Temp/DC/CheckLockedUsers.ps1
+++ DC/Temp/DC/CheckLockedUsers.ps1
@@ -18,13 +18,19 @@ Do {
     # Perform the AD account locked-out search
     Search-ADAccount -LockedOut
 
-    # Pause for user to see the results
-    pause
+    # # Pause the script to check output (uncomment for extra confirmation in script)
+    # Pause
 
     # Ask user if they want to restart or close the script
     $response = Read-Host "Would you like to [R]estart or [C]lose the script?"
-
-    # Continue the loop if the user chooses 'R', exit if 'C'
+    
+    # Clear previous output
+    Clear-Host
+# Continue the loop if the user chooses 'R', exit if 'C'
 } While ($response -eq 'R' -or $response -eq 'r')
 
+# Clear previous output
+Clear-Host
+
 Write-Host "Closing the script..."
+Start-Sleep -Seconds 1
